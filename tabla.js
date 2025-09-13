2288c6f917526b7a26daeea0edb55127627f3810
darekho <dar3kho@gmail.com>
2025-09-13 09:38:42 -0400

    Add files via upload

---
+++ tabla.js
@@ -0,0 +1,10 @@
+// tabla.js - Datos de tasas de cambio para DivisasCU
+window.tableData = [
+  { code:"EUR", label:"EUR", flag:"https://flagcdn.com/eu.svg", rate:465.00, delta:0, trend:"neutral" },
+  { code:"USD", label:"USD", flag:"https://flagcdn.com/us.svg", rate:413.00, delta:0, trend:"neutral" },
+  { code:"MLC", label:"MLC", flag:"https://eltoque.com/images/mlc.png", rate:195.00, delta:0, trend:"neutral" },
+  { code:"CAD", label:"CAD", flag:"https://flagcdn.com/ca.svg", rate:279.69, delta:+1.78, trend:"up" },
+  { code:"MXN", label:"MXN", flag:"https://flagcdn.com/mx.svg", rate:21.79, delta:-0.03, trend:"down" },
+  { code:"BRL", label:"BRL", flag:"https://flagcdn.com/br.svg", rate:74.24, delta:+0.1, trend:"up" },
+  { code:"ZELLE", label:"ZELLE", flag:"https://divisascu.vercel.app/img_2901.png", rate:411.50, delta:+1.13, trend:"up" }
+];
