0f2ca47cd22899ee3bc334e890de16396ed9cd19
darekho <dar3kho@gmail.com>
2025-09-14 16:55:37 -0400

    Update notificaciones.js

--- notificaciones.js
+++ notificaciones.js
@@ -3,11 +3,11 @@ const notificacionesData = [
   {
     titulo: "Nueva funcionalidad: Anuncios",
     descripcion: "Se a implementado un espacio publicitario para que los clientes promocionen sus marcas o negocios ante nuestra audiencia.",
-    fecha: "15/03/2023"
+    fecha: "10/09/2025"
   },
   {
     titulo: "Nueva funcionalidad: Calculadora",
     descripcion: "Hemos añadido una nueva calculadora de divisas. ¡Pruébala en la pestaña de Calculadora!",
-    fecha: "10/03/2023"
+    fecha: "02/09/2025"
   }
 ];
