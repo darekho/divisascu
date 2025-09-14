8c33131846430d7bea894a0d5de729caef746d1c
darekho <dar3kho@gmail.com>
2025-09-14 16:32:30 -0400

    Add files via upload

---
+++ notificaciones.js
@@ -0,0 +1,18 @@
+// Datos de notificaciones - Puedes editar y agregar nuevas notificaciones aquí
+const notificacionesData = [
+  {
+    titulo: "Nueva actualización de tasas",
+    descripcion: "Se han actualizado las tasas de cambio para todas las divisas. Consulta la tabla para ver los últimos valores.",
+    fecha: "15/03/2023"
+  },
+  {
+    titulo: "Mantenimiento programado",
+    descripcion: "El sitio estará en mantenimiento el próximo domingo de 2:00 AM a 4:00 AM. Disculpe las molestias.",
+    fecha: "12/03/2023"
+  },
+  {
+    titulo: "Nueva funcionalidad: Calculadora",
+    descripcion: "Hemos añadido una nueva calculadora de divisas. ¡Pruébala en la pestaña de Calculadora!",
+    fecha: "10/03/2023"
+  }
+];
