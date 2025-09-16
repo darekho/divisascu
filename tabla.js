44b24f596f9dc38c2ddf1548b01e53a01531456a
darekho <dar3kho@gmail.com>
2025-09-16 04:45:16 -0400

    Update tabla.js

--- tabla.js
+++ tabla.js
@@ -1,8 +1,8 @@
 // tabla.js - Datos de tasas de cambio para DivisasCU
 window.tableData = [
-  { code:"EUR", label:"EUR", flag:"https://flagcdn.com/eu.svg", rate:475.00, delta:+2, trend:"up" },
-  { code:"USD", label:"USD", flag:"https://flagcdn.com/us.svg", rate:420.00, delta:0, trend:"neutral" },
-  { code:"MLC", label:"MLC", flag:"https://eltoque.com/images/mlc.png", rate:203.50, delta:+3.5, trend:"up" },
+  { code:"EUR", label:"EUR", flag:"https://flagcdn.com/eu.svg", rate:475.00, delta:0, trend:"neutral" },
+  { code:"USD", label:"USD", flag:"https://flagcdn.com/us.svg", rate:421.00, delta:+1, trend:"up" },
+  { code:"MLC", label:"MLC", flag:"https://eltoque.com/images/mlc.png", rate:205.00, delta:+3.5, trend:"up" },
   { code:"CAD", label:"CAD", flag:"https://flagcdn.com/ca.svg", rate:285.22, delta:+1.6, trend:"up" },
   { code:"MXN", label:"MXN", flag:"https://flagcdn.com/mx.svg", rate:22.01, delta:+0.08, trend:"up" },
   { code:"BRL", label:"BRL", flag:"https://flagcdn.com/br.svg", rate:76.07, delta:-0.16, trend:"down" },
