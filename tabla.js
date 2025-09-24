62dc35e304e99e5333e869002fa555de38ac3017
darekho <dar3kho@gmail.com>
2025-09-24 03:19:01 -0400

    Update tabla.js

--- tabla.js
+++ tabla.js
@@ -3,8 +3,8 @@ window.tableData = [
   { code:"EUR", label:"EUR", flag:"https://flagcdn.com/eu.svg", rate:485.00, delta:0, trend:"neutral" },
   { code:"USD", label:"USD", flag:"https://flagcdn.com/us.svg", rate:428.00, delta:+1, trend:"up" },
   { code:"MLC", label:"MLC", flag:"https://eltoque.com/images/mlc.png", rate:205.00, delta:0, trend:"neutral" },
-  { code:"CAD", label:"CAD", flag:"https://flagcdn.com/ca.svg", rate:265, delta:+1.02, trend:"up" },
-  { code:"MXN", label:"MXN", flag:"https://flagcdn.com/mx.svg", rate:22.51, delta:+0.01, trend:"neutral" },
-  { code:"BRL", label:"BRL", flag:"https://flagcdn.com/br.svg", rate:75.51, delta:-0.13, trend:"down" },
-  { code:"ZELLE", label:"ZELLE", flag:"https://divisascu.vercel.app/img_2901.png", rate:423.37, delta:+0.62, trend:"up" }
+  { code:"CAD", label:"CAD", flag:"https://flagcdn.com/ca.svg", rate:265, delta:-25, trend:"down" },
+  { code:"MXN", label:"MXN", flag:"https://flagcdn.com/mx.svg", rate:22.51, delta:-0.01, trend:"down" },
+  { code:"BRL", label:"BRL", flag:"https://flagcdn.com/br.svg", rate:75.51, delta:+0.22, trend:"up" },
+  { code:"ZELLE", label:"ZELLE", flag:"https://divisascu.vercel.app/img_2901.png", rate:423.37, delta:+0.14, trend:"up" }
 ];
