38dc149790dba2d07eedf697f09b8081bda2f651
darekho <dar3kho@gmail.com>
2025-09-24 03:29:09 -0400

    Update notificaciones.js

--- notificaciones.js
+++ notificaciones.js
@@ -1,5 +1,10 @@
 // Datos de notificaciones - Puedes editar y agregar nuevas notificaciones aquí
 const notificacionesData = [
+ {
+    titulo: "Pronóstico: 1 Dólar = 500 CUP para fin de año",
+    descripcion: " Mientras finaliza septiembre, la proyección apunta a que el tipo de cambio informal superará la barrera de los 500 CUP para Diciembre. Este plazo de tres meses marca la ruta hacia un panorama económico aún más complejo para las familias.",
+    fecha: "24/09/2025"
+  },
   {
     titulo: "Nueva funcionalidad: Anuncios",
     descripcion: "Ahora ofrececemos publicidad dirigida a nuestra audiencia. Perfecto para marcas que buscan mayor visibilidad. Contáctenos para más información.",
