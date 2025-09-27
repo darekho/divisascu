1b3f507b43ccfea7ff39f1fc50740d69fcc486ce
darekho <dar3kho@gmail.com>
2025-09-27 03:40:59 -0400

    Update tabla.js

--- tabla.js
+++ tabla.js
@@ -1,10 +1,10 @@
 // tabla.js - Datos de tasas de cambio para DivisasCU
 window.tableData = [
-  { code:"EUR", label:"EUR", flag:"https://flagcdn.com/eu.svg", rate:485.00, delta:0, trend:"neutral" },
-  { code:"USD", label:"USD", flag:"https://flagcdn.com/us.svg", rate:430.00, delta:+1, trend:"up" },
-  { code:"MLC", label:"MLC", flag:"https://eltoque.com/images/mlc.png", rate:210.00, delta:+5, trend:"up" },
-  { code:"CAD", label:"CAD", flag:"https://flagcdn.com/ca.svg", rate:292, delta:+7, trend:"up" },
-  { code:"MXN", label:"MXN", flag:"https://flagcdn.com/mx.svg", rate:22.74, delta:+0.01, trend:"up" },
-  { code:"BRL", label:"BRL", flag:"https://flagcdn.com/br.svg", rate:75.86, delta:+0.16, trend:"up" },
-  { code:"ZELLE", label:"ZELLE", flag:"https://divisascu.vercel.app/img_2901.png", rate:422.07, delta:+0.43, trend:"up" }
+  { code:"EUR", label:"EUR", flag:"https://flagcdn.com/eu.svg", rate:490.00, delta:+5, trend:"up" },
+  { code:"USD", label:"USD", flag:"https://flagcdn.com/us.svg", rate:430.00, delta:0, trend:"neutral" },
+  { code:"MLC", label:"MLC", flag:"https://eltoque.com/images/mlc.png", rate:205.00, delta:-5, trend:"down" },
+  { code:"CAD", label:"CAD", flag:"https://flagcdn.com/ca.svg", rate:282.50, delta:-9.5, trend:"down" },
+  { code:"MXN", label:"MXN", flag:"https://flagcdn.com/mx.svg", rate:22.33, delta:-0.41, trend:"down" },
+  { code:"BRL", label:"BRL", flag:"https://flagcdn.com/br.svg", rate:76.04, delta:+0.18, trend:"up" },
+  { code:"ZELLE", label:"ZELLE", flag:"https://divisascu.vercel.app/img_2901.png", rate:424.071, delta:+1.94, trend:"up" }
 ];
