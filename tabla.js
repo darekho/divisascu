9a22048b1be6bdfa261dc5f2fdd453d72a01a0bb
darekho <dar3kho@gmail.com>
2025-09-28 16:58:15 -0400

    Update tabla.js

--- tabla.js
+++ tabla.js
@@ -5,6 +5,6 @@ window.tableData = [
   { code:"MLC", label:"MLC", flag:"https://eltoque.com/images/mlc.png", rate:215.00, delta:+10, trend:"up" },
   { code:"CAD", label:"CAD", flag:"https://flagcdn.com/ca.svg", rate:291.05, delta:+8.55, trend:"up" },
   { code:"MXN", label:"MXN", flag:"https://flagcdn.com/mx.svg", rate:22.50, delta:+0.17, trend:"up" },
-  { code:"BRL", label:"BRL", flag:"https://flagcdn.com/br.svg", rate:75.31, delta:-0.73, trend:"up" },
+  { code:"BRL", label:"BRL", flag:"https://flagcdn.com/br.svg", rate:75.31, delta:-0.73, trend:"down" },
   { code:"ZELLE", label:"ZELLE", flag:"https://divisascu.vercel.app/img_2901.png", rate:425.05, delta:+1.72, trend:"up" }
 ];
